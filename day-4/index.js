a41ef8d5637e4d0d7b79f49897f7a941eaf59238
Arturo <carturolopezm@gmail.com>
2023-12-12 13:49:17 -0500

    working day 4 part 2

--- day-4/index.js
+++ day-4/index.js
@@ -2,7 +2,8 @@ const readFile = require('../utils/readInput');
 
 async function day4(fileName) {
   const inputs = await readFile(fileName);
-  console.log(scratchCardsValue(inputs));
+  // console.log(scratchCardsValue(inputs));
+  console.log(scratchCardsValuesRecurring(inputs));
 }
 day4('day-4.txt');
 
@@ -33,4 +34,47 @@ const scratchCardsValue = (input) => {
     score.push(numbersMatched === 0 ? 0 : Math.pow(2, numbersMatched - 1));
   }
   return score.reduce((acc, value) => acc + value, 0);
+};
+
+const scratchCardsValuesRecurring = (input) => {
+  const cards = [];
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i];
+    const [, numbers] = line.split(':');
+    const [winningNumbersStrings, yourNumbersStrings] = numbers.split(' | ');
+    const yourNumbers = yourNumbersStrings.split(' ').filter((num) => num !== '');
+    const winningNumbersObject = {};
+    winningNumbersStrings
+      .split(' ')
+      .filter((num) => num !== '')
+      .forEach((element) => {
+        winningNumbersObject[element] = Number(element);
+      });
+    cards.push({
+      game: i,
+      amount: 1,
+      winningNumbers: winningNumbersObject,
+      yourNumbers: yourNumbers,
+    });
+  }
+  for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    let numbersMatched = 0;
+    for (const number of card.yourNumbers) {
+      if (card.winningNumbers[number]) {
+        numbersMatched++;
+      }
+    }
+
+    for (let j = i + 1; j < numbersMatched + i + 1; j++) {
+      if (j < cards.length) {
+        cards[j].amount = cards[j].amount + cards[i].amount;
+      }
+    }
+  }
+  let numberOfCards = [];
+  for (let i = 0; i < cards.length; i++) {
+    numberOfCards.push(cards[i].amount);
+  }
+  return numberOfCards.reduce((acc, value) => acc + value, 0);
 };
